5bf1fd7c73c32c4f75835c62b0d7082c8178960f
Paulo Cezar Freire Pimenta <87717926+paulinho-pimenta@users.noreply.github.com>
2026-05-29 15:44:24 -0300

    Update matriz123.c

--- matriz123.c
+++ matriz123.c
@@ -2,28 +2,26 @@
 
 int main(){
     int entrada;
-    int n = 1;
 
-    scanf("%d",&entrada);
-    int matriz[entrada][entrada];
+    while(scanf("%d",&entrada) != EOF){
+        int matriz[entrada][entrada];
 
-    for (int i = 0; i < entrada; i++){
-        for(int j = 0; j < entrada; j++){
-            if(i == j){
-                matriz[i][j] = 1;
-                printf("%d",matriz[i][j]);
-            }else if(j == entrada - n){
-                n++;
-                matriz[i][j] = 2;
-                printf("%d",matriz[i][j]);
-            }else{
-                matriz[i][j] = 3;
-                printf("%d",matriz[i][j]);
+        for (int i = 0; i < entrada; i++){
+            for(int j = 0; j < entrada; j++){
+                if(i + j == (entrada - 1)){
+                    matriz[i][j] = 2;
+                    printf("%d",matriz[i][j]);
+                }else if(i == j){
+                    matriz[i][j] = 1;
+                    printf("%d",matriz[i][j]);
+                }else{
+                    matriz[i][j] = 3;
+                    printf("%d",matriz[i][j]);
+                }
             }
+            printf("\n");
         }
-        printf("\n");
     }
-
 
     return 0;
 }
